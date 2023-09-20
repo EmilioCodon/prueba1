3e226e8fba1753c0679aee2736b2f71cccdb7bd9
EmilioCodon <emiliocodon@gmail.com>
2023-09-20 17:22:17 +0200

    4000

--- proxy.js
+++ proxy.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const fetch = require('node-fetch');
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 4000;
 
 // Ruta para redirigir solicitudes a la API de noticias
 app.get('/api/news', async (req, res) => {
