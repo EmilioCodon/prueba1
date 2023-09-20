cc8eca4e009b86b0763f6421ebf5ecb5e5190aa2
EmilioCodon <emiliocodon@gmail.com>
2023-09-20 18:06:33 +0200

    conserver400

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,4 +1,4 @@
-/* import React, { Component } from "react";
+import React, { Component } from "react";
 import News from "./News";
 
 class Blog extends Component {
@@ -12,7 +12,7 @@ state = {
   }
 
   consultaNoticias = () => {
-   let url = 'https://newsapi.org/v2/top-headlines?country=us&category=technology&apiKey=d766bc99f3834102b779f9296aabaf73'; 
+   /*  let url = 'https://newsapi.org/v2/top-headlines?country=us&category=technology&apiKey=d766bc99f3834102b779f9296aabaf73'; */
     let url = '/api/news'; 
     fetch(url)
       .then(respuesta => {
@@ -29,54 +29,6 @@ state = {
   render() {
     return (
       <News noticias={this.state.noticias}/>
-    );
-  }
-}
-
-export default Blog;
- */
-
-import React, { Component } from "react";
-
-const NewsAPI = require('newsapi');
-const newsapi = new NewsAPI('d766bc99f3834102b779f9296aabaf73');
-
-class Blog extends Component {
-  state = {
-    noticias: []
-  }
-
-  componentDidMount() {
-    // Ejemplo de solicitud a /v2/top-headlines
-    newsapi.v2.topHeadlines({
-      sources: 'bbc-news,the-verge',
-      q: 'bitcoin',
-      category: 'business',
-      language: 'en',
-      country: 'us'
-    })
-    .then(response => {
-      console.log(response);
-      this.setState({
-        noticias: response.articles
-      });
-    })
-    .catch(error => {
-      console.error(error);
-    });
-  }
-
-  render() {
-    return (
-      <div>
-        {this.state.noticias.map((noticia, index) => (
-          <div key={index}>
-            <h2>{noticia.title}</h2>
-            <p>{noticia.description}</p>
-            {/* Agrega más detalles de la noticia según tus necesidades */}
-          </div>
-        ))}
-      </div>
     );
   }
 }
