cdc23207c98a3e868a794a6796752953f6010826
EmilioCodon <emiliocodon@gmail.com>
2023-09-21 18:18:15 +0200

    nuevaapi

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -2,34 +2,34 @@ import React, { Component } from "react";
 import News from "./News";
 
 class Blog extends Component {
-
-state = {
-  noticias : []
-}
+  state = {
+    noticias: [],
+  };
 
   componentDidMount() {
     this.consultaNoticias();
   }
 
   consultaNoticias = () => {
-     let url = 'https://newsapi.org/v2/top-headlines?country=us&category=technology&apiKey=d766bc99f3834102b779f9296aabaf73';
-   /*  let url = '/api/news';  */
+
+   let url ="https://gnews.io/api/v4/top-headlines?category=technology&lang=en&apikey=9e599b5dacaf1b9993e0d8a73a3b1741";
+    /* 
+      let url = 'https://gnews.io/api/v4/search?q=example&lang=en&country=us&max=10&apikey='; */
+    /*    let url = '/api/news';    */
     fetch(url)
-      .then(respuesta => {
+      .then((respuesta) => {
         return respuesta.json();
       })
-      .then(noticias => {
+      .then((noticias) => {
         console.log(noticias);
         this.setState({
-          noticias: noticias.articles
-        })
+          noticias: noticias.articles,
+        });
       });
-  }
+  };
 
   render() {
-    return (
-      <News noticias={this.state.noticias}/>
-    );
+    return <News noticias={this.state.noticias} />;
   }
 }
 
--- src/components/new.js
+++ src/components/new.js
@@ -1,13 +1,13 @@
 import React from "react";
 
 const New = (props) => {
-  const { url, author, title, urlToImage, content } = props.noticia;
+  const { url, author, title, image, content } = props.noticia;
 
   return (
     
       <div class="news-container">
         <div class="news-item">
-          <img src={urlToImage} alt={title} class="new-image" />
+          <img src={image} alt={title} class="new-image" />
           <span class="new-author">{author}</span>
           <h5 class="new-title">{title}</h5>
           <p class="new-content">{content}</p>
